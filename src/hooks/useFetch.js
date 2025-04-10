cdc7027247c24bdf36958ceb9298f2e3881f7b04
The-Wee-Lad <113277834+The-Wee-Lad@users.noreply.github.com>
2025-04-10 22:15:48 +0530

    revert

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,20 +1,23 @@
-import { useEffect, useState } from "react";
-function useFetch(currency, tryAgain, setTryAgain) {
+import { useCallback, useState } from "react";
+function useFetch(currency) {
     const [data, setData] = useState({})
-    useEffect(() => {
-        fetch(`https://api.exchangerate-api.com/v4/latest/${currency}`)
+    const [isError, setIsError] = useState(false);
+
+    const fetchRates = useCallback(async () => {
+        await fetch(`https://api.exchangerate-api.com/v4/latest/${currency}`)
             .then((response => {
-                console.log("response",response.status);
-                if(response.status==404)
+                console.log("response", response.status);
+                if (response.status == 404)
                     throw new Error("API Failed, try Again!!!");
                 return response.json();
             }))
             .then((response) => setData(response))
             .catch((err) => {
                 console.log(err);
-                setTryAgain(true);
+                setIsError(true);
             });
-    }, [currency, tryAgain, setTryAgain]);
-    return data;
+
+    }, [currency]);
+    return { data, fetchRates, isError };
 }
 export default useFetch
