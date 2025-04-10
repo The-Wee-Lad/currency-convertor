55372179ffe70a871e34a9b46863f9aba83d1ee1
The-Wee-Lad <113277834+The-Wee-Lad@users.noreply.github.com>
2025-04-11 03:55:35 +0530

    add 500ms debouncer to limit requests

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,19 +1,24 @@
 import { useEffect, useState } from "react";
+
+const debouncingRate = 500
 function useFetch(currency, tryAgain, setTryAgain) {
     const [data, setData] = useState({})
     useEffect(() => {
-        fetch(`https://api.exchangerate-api.com/v4/latest/${currency}`)
-            .then((response => {
-                console.log("response",response.status);
-                if(response.status==404)
-                    throw new Error("API Failed, try Again!!!");
-                return response.json();
-            }))
-            .then((response) => setData(response))
-            .catch((err) => {
-                console.log(err);
-                setTryAgain(true);
-            });
+        const debouncer = setTimeout(() => {
+            fetch(`https://api.exchangerate-api.com/v4/latest/${currency}`)
+                .then((response => {
+                    console.log("response", response.status);
+                    if (response.status == 404)
+                        throw new Error("API Failed, try Again!!!");
+                    return response.json();
+                }))
+                .then((response) => setData(response))
+                .catch((err) => {
+                    console.log(err);
+                    setTryAgain(true);
+                });
+        }, debouncingRate);
+        return () => {clearTimeout(debouncer)}
     }, [currency, tryAgain, setTryAgain]);
     return data;
 }
