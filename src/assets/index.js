43d7c39520fe6cbcfa7eb7ee96276e309a76e9c6
The-Wee-Lad <113277834+The-Wee-Lad@users.noreply.github.com>
2025-04-11 11:41:46 +0530

    remove image 6

--- src/assets/index.js
+++ src/assets/index.js
@@ -5,7 +5,7 @@ import img3 from './3-min.jpg';
 import img4 from './4-min.jpg';
 import img5 from './5-min.jpg';
 
-import img6 from './6-min.jpg';
+// import img6 from './6-min.jpg';
 import dark1 from './7-min.jpg';
 import dark2 from './8-min.jpg';
 // import dark3 from './9-min.jpg';
@@ -13,6 +13,6 @@ import dark4 from './10-min.jpg';
 import dark5 from './11-min.jpg';
 import dark6 from './12-min.jpg';
 
-let lightArray = [img1, img2, img3, img4, img5,img6];
+let lightArray = [img1, img2, img3, img4, img5];
 let darkArray = [dark1, dark2, dark4, dark5,dark6];
 export {lightArray, darkArray};
