d0144b886c4eedce132e6bcef50d49b0b6db41ee
The-Wee-Lad <113277834+The-Wee-Lad@users.noreply.github.com>
2025-04-11 11:38:19 +0530

    remove image 9

--- src/assets/index.js
+++ src/assets/index.js
@@ -8,11 +8,11 @@ import img5 from './5-min.jpg';
 import img6 from './6-min.jpg';
 import dark1 from './7-min.jpg';
 import dark2 from './8-min.jpg';
-import dark3 from './9-min.jpg';
+// import dark3 from './9-min.jpg';
 import dark4 from './10-min.jpg';
 import dark5 from './11-min.jpg';
 import dark6 from './12-min.jpg';
 
 let lightArray = [img1, img2, img3, img4, img5,img6];
-let darkArray = [dark1, dark2, dark3, dark4, dark5,dark6];
+let darkArray = [dark1, dark2, dark4, dark5,dark6];
 export {lightArray, darkArray};
